2c690ff13df36baa0bc23d76275820dffd499ccf
Tamanna@5092 <tamannaaktermoni51@gmail.com>
2024-06-01 20:39:30 +0600

    content added

---
+++ src/components/Author.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Author = () => {
+    const blog = useLoaderData()
+    return (
+        <div>
+            <h1>{blog.title}</h1>
+        </div>
+    );
+};
+
+export default Author;
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import Home from './Pages/Home.jsx'
 import Blogs from './Pages/Blogs.jsx'
 import Bookmarks from './Pages/Bookmarks.jsx'
 import Layout from './Layout/Layout.jsx'
+import Blog from './Pages/Blog.jsx'
+import Content from './components/Content.jsx'
+import Author from './components/Author.jsx'
+
 const router = createBrowserRouter([
     {
         path: '/',
@@ -19,6 +23,23 @@ const router = createBrowserRouter([
                 path: '/blogs',
                 element: <Blogs></Blogs>,
                 loader: () => fetch('https://dev.to/api/articles?per_page=20&top=7'),
+            },
+            {
+                path: '/blog/:id',
+                element: <Blog></Blog>,
+                loader: ({params}) => fetch(`https://dev.to/api/articles/${params.id}`),
+                children: [
+                    {
+                    index: true,
+                    element: <Content></Content>,
+                    loader: ({params}) => fetch(`https://dev.to/api/articles/${params.id}`)
+                    },
+                    {
+                        path: 'author',
+                        element: <Author></Author>,
+                        loader: ({params}) => fetch(`https://dev.to/api/articles/${params.id}`)
+                    }
+                ]
             },
             {
                 path: '/bookmarks',
